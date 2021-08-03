18f06fb80ee503ceef8bfd4396cab368f083dccb
MirajSYM <mirajahmedsymanto@gmail.com>
2021-08-04 07:44:51 +0800

    first commit

---
+++ lib/providers/auth.dart
@@ -0,0 +1,104 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/http_exception.dart';
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expiryDate;
+  String _userId;
+
+  bool get isAuth {
+    return token != null;
+  }
+
+  String get token {
+    if (_expiryDate != null &&
+        _expiryDate.isAfter(DateTime.now()) &&
+        _token != null) {
+      return _token;
+    }
+    return null;
+  }
+
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url = Uri.parse(
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/$urlSegment?key=AIzaSyDzJx_ddbKPVsf9aqmC2HZZ77n0ITWB2Ns');
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+      _token = responseData['idToken'];
+      _userId = responseData['localId'];
+      _expiryDate = DateTime.now().add(
+        Duration(
+          seconds: int.parse(
+            responseData['expiresIn'],
+          ),
+        ),
+      );
+      notifyListeners();
+      final prefs = await SharedPreferences.getInstance();
+      final userData = json.encode(
+        {
+          'token': _token,
+          'userId': _userId,
+          'expiryDate': _expiryDate.toIso8601String()
+        },
+      );
+      prefs.setString('userData', userData);
+    } catch (error) {
+      return Future.error(error);
+    }
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signupNewUser');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'verifyPassword');
+  }
+
+  Future<bool> autoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (!prefs.containsKey('userData')) {
+      return false;
+    }
+    final extractedUserData =
+        prefs.getString(('userData')) as Map<String, Object>;
+    final expiryData = DateTime.parse(extractedUserData['expiryDate']);
+    if (expiryData.isAfter(DateTime.now())) {
+      return false;
+    }
+    _token = extractedUserData['token'];
+    _userId = extractedUserData['userId'];
+    _expiryDate = expiryData;
+    notifyListeners();
+    return true;
+  }
+
+  Future<void> logout() async {
+    _token = null;
+    _userId = null;
+    _expiryDate = null;
+
+    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+  }
+}
